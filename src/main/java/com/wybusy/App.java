25b5ac26072556b78d57ddd9c92b816f903ad437
wy <wy@world.com>
2021-09-12 16:56:59 +0800

    1.0.1新增zip文件的压缩，解压

--- src/main/java/com/wybusy/App.java
+++ src/main/java/com/wybusy/App.java
@@ -10,12 +10,12 @@ public class App
     {
 //        EasyFile.unzip("/qs-server-1.0-SNAPSHOT.zip", "/unzip");
 //        EasyFile.unzip("/初中学业考试.zip", "/unzip");
-        try {
-        EasyFile.zip("/初中学业考试", "/unzip.zip");
-        } catch (Exception e){
-            System.out.println(e);
-        }
-
+//        try {
+//        EasyFile.zip("/初中学业考试", "/unzip.zip");
+//        } catch (Exception e){
+//            System.out.println(e);
+//        }
+//
         System.out.println( "Hello EasyFile!" );
     }
 }
